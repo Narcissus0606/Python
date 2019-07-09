b20341ba8aed4b469d134867f422b7556f17c112
Narcissus0606 <2497672933@qq.com>
2019-07-09 20:25:33 +0800

    0709

---
+++ bs_test_01.py
@@ -0,0 +1,13 @@
+#BeautifulSoup
+#引入这个类
+
+from bs4 import BeautifulSoup
+import requests
+url = "http://192.168.104.1:8080/zh/1.html"
+resp = requests.get(url)
+html = resp.text
+soup = BeautifulSoup(html,"html.parser")
+title_list = soup.findAll("title")
+print(type(title_list))
+for tag in title_list:
+    print(tag.get)
