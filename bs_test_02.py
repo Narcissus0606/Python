ee5b840d91371d868db8e10c21d429486e7cb4a7
Yu Shi Tang <yu.shi.tang@ibm.com>
2023-12-21 11:00:22 +0800

    2nd commit

--- bs_test_02.py
+++ bs_test_02.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-
+#test
 url="http://192.168.104.1:8080/zh/2018%E4%B8%AD%E5%9B%BD%E6%9C%80%E5%A5%BD%E5%A4%A7%E5%AD%A6%E6%8E%92%E5%90%8D_%E6%9C%80%E5%A5%BD%E5%A4%A7%E5%AD%A6%E7%BD%91.html"
 resp = requests.get(url)
 resp.encoding = "UTF-8"
